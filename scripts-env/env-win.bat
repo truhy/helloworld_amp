890cfec2283ed9a1acc003678aa03adae5295d9c
Truong Hy <truhy7@gmail.com>
2024-10-03 13:30:58 +0100

    Corrected U-Boot QTS folder location to terasic

--- scripts-env/env-win.bat
+++ scripts-env/env-win.bat
@@ -1,27 +1,29 @@
+@ECHO OFF
+
 :: Get the parent folder path (one level up) from this script's path
-@FOR %%i IN ("%~dp0..") DO @SET "SCRIPT_PATH=%%~fi"
-::@FOR %%i IN ("%~dp0..") DO @SET "SCRIPT_PATH=%%~dpi"
+FOR %%i IN ("%~dp0..") DO SET "SCRIPT_PATH=%%~fi"
+::FOR %%i IN ("%~dp0..") DO SET "SCRIPT_PATH=%%~dpi"
 
 :: Tools settings
-@SET MAKE_PATH=C:\devtools\xpack-windows-build-tools-4.4.1-2\bin
-@SET TOOLCHAIN_PATH=C:\devtools\xpack-arm-none-eabi-gcc-13.2.1-1.1\bin
-@SET OPENOCD_PATH=C:\devtools\xpack-openocd-0.12.0-2\bin
+SET MAKE_PATH=C:\devtools\xpack-windows-build-tools-4.4.1-2\bin
+SET TOOLCHAIN_PATH=C:\devtools\xpack-arm-none-eabi-gcc-13.2.1-1.1\bin
+SET OPENOCD_PATH=C:\devtools\xpack-openocd-0.12.0-2\bin
 
 :: Bare-metal settings
-@SET BM_OUT_PATH=%SCRIPT_PATH%
-@SET BM_HOME_PATH=%SCRIPT_PATH%
-@SET BM_SRC_PATH1=source/app1
-@SET BM_PROGRAM_NAME1=hwamp_app1
-@SET BM_SRC_PATH2=source/app2
-@SET BM_PROGRAM_NAME2=hwamp_app2
+SET BM_OUT_PATH=%SCRIPT_PATH%
+SET BM_HOME_PATH=%SCRIPT_PATH%
+SET BM_SRC_PATH1=source/app1
+SET BM_PROGRAM_NAME1=hwamp_app1
+SET BM_SRC_PATH2=source/app2
+SET BM_PROGRAM_NAME2=hwamp_app2
 
 :: Search path settings
-@SET path=%path%;%SCRIPT_PATH%\scripts-env;%SCRIPT_PATH%\scripts-win
-@IF "%MAKE_PATH%" NEQ "" SET path=%path%;%MAKE_PATH%
-@IF "%TOOLCHAIN_PATH%" NEQ "" SET path=%path%;%TOOLCHAIN_PATH%
-@IF "%OPENOCD_PATH%" NEQ "" SET path=%path%;%OPENOCD_PATH%
+SET path=%path%;%SCRIPT_PATH%\scripts-env;%SCRIPT_PATH%\scripts-win
+IF "%MAKE_PATH%" NEQ "" SET path=%path%;%MAKE_PATH%
+IF "%TOOLCHAIN_PATH%" NEQ "" SET path=%path%;%TOOLCHAIN_PATH%
+IF "%OPENOCD_PATH%" NEQ "" SET path=%path%;%OPENOCD_PATH%
 
 :: Messages
-@IF "%MAKE_PATH%" NEQ ""      ECHO Make     : %MAKE_PATH%
-@IF "%TOOLCHAIN_PATH%" NEQ "" ECHO Toolchain: %TOOLCHAIN_PATH%
-@IF "%OPENOCD_PATH%" NEQ ""   ECHO OpenOCD  : %OPENOCD_PATH%
+IF "%MAKE_PATH%" NEQ ""      ECHO Make     : %MAKE_PATH%
+IF "%TOOLCHAIN_PATH%" NEQ "" ECHO Toolchain: %TOOLCHAIN_PATH%
+IF "%OPENOCD_PATH%" NEQ ""   ECHO OpenOCD  : %OPENOCD_PATH%
--- scripts-linux/uboot/Makefile-prep-ub.mk
+++ scripts-linux/uboot/Makefile-prep-ub.mk
@@ -1,5 +1,5 @@
 # This is free script released into the public domain.
-# GNU make file v20240424 created by Truong Hy.
+# GNU make file v20241003 created by Truong Hy.
 #
 # Prepares the U-Boot source files for building U-Boot for the Intel Cyclone V SoC:
 #   - uncompress U-Boot source
@@ -15,17 +15,20 @@
 ifndef UBOOT_ZIP
 $(error UBOOT_ZIP environment variable is not set)
 endif
-
 ifndef UBOOT_OUT_PATH
 $(error UBOOT_OUT_PATH environment variable not set)
 endif
-
 ifndef UBOOT_PATCH_FOLDER
 $(error UBOOT_PATCH_FOLDER environment variable not set)
 endif
-
 ifndef UBOOT_BSP_GEN_FOLDER
 $(error UBOOT_BSP_GEN_FOLDER environment variable not set)
+endif
+ifndef UBOOT_DEFCONFIG
+$(error UBOOT_DEFCONFIG environment variable not set)
+endif
+ifndef UBOOT_QTS_FOLDER
+$(error UBOOT_QTS_FOLDER environment variable not set)
 endif
 
 # Check if unzip command exists
@@ -45,10 +48,11 @@ REL_UBOOT_SRC_PATH := $(REL_UBOOT_OUT_PATH)/u-boot
 # ================================
 # U-Boot replacment modified files
 # ================================
-DBG_SRC_FILE1 := Debug/$(UBOOT_PATCH_FOLDER)/socfpga_de10_nano_defconfig
-DBG_DST_FILE1 := $(DBG_UBOOT_SRC_PATH)/configs/socfpga_de10_nano_defconfig
-REL_SRC_FILE1 := Release/$(UBOOT_PATCH_FOLDER)/socfpga_de10_nano_defconfig
-REL_DST_FILE1 := $(REL_UBOOT_SRC_PATH)/configs/socfpga_de10_nano_defconfig
+
+DBG_SRC_FILE1 := Debug/$(UBOOT_PATCH_FOLDER)/$(UBOOT_DEFCONFIG)
+DBG_DST_FILE1 := $(DBG_UBOOT_SRC_PATH)/configs/$(UBOOT_DEFCONFIG)
+REL_SRC_FILE1 := Release/$(UBOOT_PATCH_FOLDER)/$(UBOOT_DEFCONFIG)
+REL_DST_FILE1 := $(REL_UBOOT_SRC_PATH)/configs/$(UBOOT_DEFCONFIG)
 
 # ============
 # BSP settings
@@ -56,8 +60,8 @@ REL_DST_FILE1 := $(REL_UBOOT_SRC_PATH)/configs/socfpga_de10_nano_defconfig
 
 ALTERA_BSP_SCRIPT := $(UBOOT_BSP_GEN_FOLDER)/cv_bsp_generator.py
 HANDOFF := hps_isw_handoff/soc_system_hps_0
-DBG_UBOOT_QTS_PATH := $(DBG_UBOOT_SRC_PATH)/board/altera/cyclone5-socdk/qts
-REL_UBOOT_QTS_PATH := $(REL_UBOOT_SRC_PATH)/board/altera/cyclone5-socdk/qts
+DBG_UBOOT_QTS_PATH := $(DBG_UBOOT_SRC_PATH)/$(UBOOT_QTS_FOLDER)
+REL_UBOOT_QTS_PATH := $(REL_UBOOT_SRC_PATH)/$(UBOOT_QTS_FOLDER)
 
 # ==========================================================================================
 # Dummy prep file so that we have a timestamp for satisfying GNU make's prerequite condition
